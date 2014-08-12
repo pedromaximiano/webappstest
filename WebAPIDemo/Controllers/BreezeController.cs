f54b6995f2a6de40849fe2b958983d13d72c3604
pedromaximiano <pedro.maximiano@gmail.com>
2014-08-12 19:42:34 +0100

    Installs Breeze

---
+++ WebAPIDemo/Controllers/BreezeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Breeze.WebApi2;
+
+namespace WebAPIDemo.Controllers
+{
+    [BreezeController]
+    public class BreezeController : ApiController
+    {
+    }
+}
